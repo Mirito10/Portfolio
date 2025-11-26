5d4428c8ebb6638e8e35e753d9786033ea2da7c1
Mirito10 <157768105+Mirito10@users.noreply.github.com>
2025-11-26 12:20:46 -0600

    feat: update About icons

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,4 +1,4 @@
-import { Briefcase, Code, User } from "lucide-react";
+import { Bot, TextSearch, Cloud } from "lucide-react";
 
 export const AboutSection = () => {
     return (
@@ -45,7 +45,7 @@ export const AboutSection = () => {
                         <div className="gradient-border p-6 card-hover">
                             <div className="flex items-start gap-4">
                                 <div className="p-3 rounded-full bg-primary/10">
-                                    <Code className="h-6 w-6 text-primary"/>
+                                    <Cloud className="h-6 w-6 text-primary"/>
                                 </div>
                                 <div className="text-left">
                                     <h4 className="font-semibold text-lg">
@@ -61,7 +61,7 @@ export const AboutSection = () => {
                         <div className="gradient-border p-6 card-hover">
                             <div className="flex items-start gap-4">
                                 <div className="p-3 rounded-full bg-primary/10">
-                                    <User className="h-6 w-6 text-primary"/>
+                                    <Bot className="h-6 w-6 text-primary"/>
                                 </div>
                                 <div className="text-left">
                                     <h4 className="font-semibold text-lg">
@@ -77,7 +77,7 @@ export const AboutSection = () => {
                         <div className="gradient-border p-6 card-hover">
                             <div className="flex items-start gap-4">
                                 <div className="p-3 rounded-full bg-primary/10">
-                                    <Briefcase className="h-6 w-6 text-primary"/>
+                                    <TextSearch className="h-6 w-6 text-primary"/>
                                 </div>
                                 <div className="text-left">
                                     <h4 className="font-semibold text-lg">
