00bbff0d0965961ce06856bcca637147d3ff1895
Mirito10 <calderonmathias6013@gmail.com>
2025-12-03 23:09:06 -0600

    Remove extra © symbol from footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { ArrowUp } from "lucide-react";
 export const Footer = () => {
     return (
         <footer className=" py-12 px-4 bg-card/75 relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center">
-            <p className="text-sm text-muted-foreground"> &copy; © {new Date().getFullYear()} MiritoTech.co. WEBSITE UNDER DEVELOPMENT</p>
+            <p className="text-sm text-muted-foreground"> &copy; {new Date().getFullYear()} MiritoTech.co. WEBSITE UNDER DEVELOPMENT</p>
             <a href="#hero" className="p-2 rounded-full bg-primary/10 hover:bg-primary/20 text-primary transition-colors"> <ArrowUp size={20}/></a>
         </footer>
     )
